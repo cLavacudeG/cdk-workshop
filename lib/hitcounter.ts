121a2e20c75acfb66f008932616cf4a78a4c3077
cdlavacudeg <cristian.lavacude@anthology.com>
2024-04-05 09:24:59 -0500

    Validation Test and describe

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -6,6 +6,14 @@ import { Construct } from "constructs";
 export interface HitCounterProps {
   // ** Function for which we want to count url hist **/
   downstream: lambda.IFunction;
+  /**
+   * The read capacity for the DynamoDB table.
+   *
+   * Must be greater than 5 and lower than 20
+   *
+   * @default 5
+   */
+  readCapacity?: number;
 }
 
 export class HitCounter extends Construct {
@@ -14,12 +22,19 @@ export class HitCounter extends Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
+    if (
+      props.readCapacity !== undefined &&
+      (props.readCapacity < 5 || props.readCapacity > 20)
+    ) {
+      throw new Error("readCapacity must be between 5 and 20");
+    }
     super(scope, id);
 
     const table = new dynamodb.Table(this, "Hits", {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      readCapacity: props.readCapacity ?? 5,
     });
 
     this.table = table;
--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -3,78 +3,96 @@ import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 
 import { HitCounter } from "../lib/hitcounter";
-import { Environment } from "aws-cdk-lib/aws-appconfig";
 
-test("DynamoDB Table Created", () => {
-  const stack = new cdk.Stack();
+describe("DynamoDB", () => {
+  test("DynamoDB Table Created", () => {
+    const stack = new cdk.Stack();
 
-  // WHEN
+    // WHEN
 
-  new HitCounter(stack, "HitCounterTestConstruct", {
-    downstream: new lambda.Function(stack, "TestFunction", {
-      runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
-      code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
-      handler: "hello.handler",
-    }),
+    new HitCounter(stack, "HitCounterTestConstruct", {
+      downstream: new lambda.Function(stack, "TestFunction", {
+        runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
+        code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
+        handler: "hello.handler",
+      }),
+    });
+
+    // THEN
+
+    const template = Template.fromStack(stack);
+    template.resourceCountIs("AWS::DynamoDB::Table", 1);
   });
 
-  // THEN
+  test("DynamoDB Table Created With Encryption", () => {
+    const stack = new cdk.Stack();
 
-  const template = Template.fromStack(stack);
-  template.resourceCountIs("AWS::DynamoDB::Table", 1);
+    // WHEN
+    new HitCounter(stack, "HitCounterTestConstruct", {
+      downstream: new lambda.Function(stack, "TestFunction", {
+        runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
+        code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
+        handler: "hello.handler",
+      }),
+    });
+
+    //THEN
+
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties("AWS::DynamoDB::Table", {
+      SSESpecification: {
+        SSEEnabled: true,
+      },
+    });
+  });
+
+  test("DynamoDB Table read capacity can be configured", () => {
+    const stack = new cdk.Stack();
+
+    expect(() => {
+      new HitCounter(stack, "HitCounterTestConstruct", {
+        downstream: new lambda.Function(stack, "TestFunction", {
+          runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
+          code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
+          handler: "hello.handler",
+        }),
+        readCapacity: 3,
+      });
+    }).toThrowError("readCapacity must be between 5 and 20");
+  });
 });
 
-test("DynamoDB Table Created With Encryption", () => {
-  const stack = new cdk.Stack();
+describe("Lambda", () => {
+  test("Lambda Has Environment Variables", () => {
+    const stack = new cdk.Stack();
 
-  // WHEN
-  new HitCounter(stack, "HitCounterTestConstruct", {
-    downstream: new lambda.Function(stack, "TestFunction", {
-      runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
-      code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
-      handler: "hello.handler",
-    }),
-  });
+    // WHEN
+    new HitCounter(stack, "HitCounterTestConstruct", {
+      downstream: new lambda.Function(stack, "TestFunction", {
+        runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
+        code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
+        handler: "hello.handler",
+      }),
+    });
 
-  //THEN
+    // THEN
+    const template = Template.fromStack(stack);
+    const envCapture = new Capture();
 
-  const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::Lambda::Function", {
+      Environment: envCapture,
+    });
 
-  template.hasResourceProperties("AWS::DynamoDB::Table", {
-    SSESpecification: {
-      SSEEnabled: true,
-    },
-  });
-});
-
-test("Lambda Has Environment Variables", () => {
-  const stack = new cdk.Stack();
-
-  // WHEN
-  new HitCounter(stack, "HitCounterTestConstruct", {
-    downstream: new lambda.Function(stack, "TestFunction", {
-      runtime: lambda.Runtime.NODEJS_16_X, // Execution environment
-      code: lambda.Code.fromAsset("lambda"), // Code loaded from lambda folder in root
-      handler: "hello.handler",
-    }),
-  });
-
-  // THEN
-  const template = Template.fromStack(stack);
-  const envCapture = new Capture();
-
-  template.hasResourceProperties("AWS::Lambda::Function", {
-    Environment: envCapture,
-  });
-
-  expect(envCapture.asObject()).toEqual({
-    Variables: {
-      DOWNSTREAM_FUNCTION_NAME: {
-        Ref: "TestFunction22AD90FC",
+    expect(envCapture.asObject()).toEqual({
+      Variables: {
+        DOWNSTREAM_FUNCTION_NAME: {
+          Ref: "TestFunction22AD90FC",
+        },
+        HITS_TABLE_NAME: {
+          Ref: "HitCounterTestConstructHitsB2E940F2",
+        },
       },
-      HITS_TABLE_NAME: {
-        Ref: "HitCounterTestConstructHitsB2E940F2",
-      },
-    },
+    });
   });
 });
